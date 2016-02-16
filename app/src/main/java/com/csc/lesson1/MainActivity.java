9a88e21d8a1ecdc44f6467a2d324341f9091d10a
jamsic <jamsic@yandex.ru>
2016-02-17 01:01:47 +0300

    task

--- app/src/main/java/com/csc/lesson1/MainActivity.java
+++ app/src/main/java/com/csc/lesson1/MainActivity.java
@@ -1,12 +1,60 @@
 package com.csc.lesson1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+        //        .execute("http://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
+        //Initialize ImageView
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+//Loading image from below url into imageView
+
+        Picasso.with(this)
+                .load("http://www.alphasoftware.com/blog/wp-content/uploads/2015/12/Android.jpg")
+                .into(imageView);
     }
+
+    /*
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+    */
 }
